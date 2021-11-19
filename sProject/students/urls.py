de2787b5d5e53c780a443d59ad3bdc9376199379
GyuHwan Jung <foxrain87@naver.com>
2021-11-19 17:52:22 +0900

    final commit

--- sProject/students/urls.py
+++ sProject/students/urls.py
@@ -1,7 +1,7 @@
 """studentsProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from . import views
 from django.urls import path
-
+from . import views
 
 app_name = 'students'
 urlpatterns = [
     path('reg/', views.regStudent, name='reg'),
-    path('regCon/', views.regConStudent, name='regCon'),
-    path('all/', views.reaStudentAll, name='stuAll'),
-
+	path('regCon/', views.regConStudent, name='regCon'),
+	path('all/', views.reaStudentAll, name='stuAll'),
+	path('<str:name>/det/', views.detStudent, name='stuDet'),
+	path('<str:name>/mod/', views.reaStudentOne, name='stuMod'),
+	path('modCon/', views.modConStudent, name='modCon'),
+	path('<str:name>/del', views.delConStudent, name='stuDel'),
 ]
